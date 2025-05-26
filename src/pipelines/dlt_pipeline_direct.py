30f5cd2b3e7a49f9d3e8bd0343661a70d0001596
j4.anhnn <j4.anhnn>
2025-05-26 08:32:27 +0000

    commit

--- src/pipelines/dlt_pipeline_direct.py
+++ src/pipelines/dlt_pipeline_direct.py
@@ -4,11 +4,13 @@ from pyspark.sql.types import StringType
 
 @dlt.table(name="opportunity_raw_batch")
 def opportunity_raw_batch():
-    csv_path = "dbfs:/FileStore/tables/sales_data.csv" 
+    csv_path = "dbfs:/FileStore/tables/sales_data.csv"
+    
     df = spark.read.option("header", True).option("inferSchema", True).csv(csv_path)
     
     for c in ["Closed_Opportunity", "Active_Opportunity", "Latest_Status_Entry"]:
         df = df.withColumn(c, when(col(c) == "TRUE", True).otherwise(False))
+    
     df = df.withColumn("Date", to_date(col("Date"), "M/d/yyyy"))
     df = df.withColumn("Target_Close", to_date(col("Target_Close"), "M/d/yyyy"))
     
